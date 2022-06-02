c5e30b8a7cd333e0628b0a692aed95cde32ad4c3
henryclee <henryclee73@gmail.com>
2022-06-02 11:15:28 -0400

    EventSeqVector done, not tested

--- src/index/EventSeqVector.scala
+++ src/index/EventSeqVector.scala
@@ -1,5 +1,34 @@
 package index
 
-class EventSeqVector {
+class EventSeqVector (val k: Int, val kmerMap: Map [String,Double], sequence: Array[Char]) {
+
+  def toVectors (dim: Int): Array[Array[Double]] = {
+
+    //Number of kmers in the sequence
+    val kmerNo: Int = sequence.length - k + 1
+    val vectorNo: Int = sequence.length - k - dim + 2
+
+    var kmerSeq: Array[String] = new Array[String](kmerNo)
+    var eventSeq: Array[Double] = new Array[Double](kmerNo)
+    var vectorSet: Array[Array[Double]] = new Array[Array[Double]](vectorNo)
+
+    //This loop populates eventSeq with the events from the sequence
+    for (i <- 0 until kmerNo) {
+      var kmer: String = ""
+      for (j <- 0 until k) {
+        kmer += sequence(i+j)
+      }
+      eventSeq(i) = kmerMap(kmer)
+    }
+
+    //This loop populates the vectorSet with events as tuples
+    for (i <- 0 until vectorNo) {
+      for (j <- 0 until dim) {
+        vectorSet(i)(j) = eventSeq(i+j)
+      }
+    }
+    vectorSet
+  }
+
 
 }
--- src/test/Index.scala
+++ src/test/Index.scala
@@ -4,4 +4,33 @@ import org.scalatest._
 
 class Index extends FunSuite {
 
+  test("Indexing") {
+
+    var kmerMap: Map[String, Double] = Map()
+    val nucleotide: Array [String] = Array ("A","T","C","G")
+
+    // expected current for the k-mer-expected current mapping is defined arbitrarily between 0-1023
+    var current: Double = 0.0
+
+    //populating the k-mer-expected event for a 5-mer
+    for (i <- 0 to 3) {
+      for (j <- 0 to 3) {
+        for (k <- 0 to 3) {
+          for (l <- 0 to 3) {
+            for (m <- 0 to 3) {
+              val kmer: String = nucleotide(i) + nucleotide(j) + nucleotide(k) + nucleotide(l) + nucleotide(m)
+              kmerMap = kmerMap + (kmer -> current)
+              current += 1.0
+            }
+          }
+        }
+      }
+    }
+
+
+
+
+
+  }
+
 }
