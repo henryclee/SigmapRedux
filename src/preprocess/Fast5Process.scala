bf49e3fa080d6c99db087f65d5723360e27f2b06
henryclee <henryclee73@gmail.com>
2022-06-16 19:59:40 -0400

    Implemented segmentation in preprocessing

--- src/preprocess/Fast5Process.scala
+++ src/preprocess/Fast5Process.scala
@@ -45,15 +45,160 @@ object Fast5Process {
     signalList.reverse
   }
 
+  //Helper function to create a list of arrays. Array will hold 2 values: mean, std dev
+  //Parameters will be the fast5 signal as a list of doubles, and the window size
+  def makeReferenceArray(eventArray: Array[Double], window: Int) : Array[Array[Double]] = {
+
+    val numWindows: Int = eventArray.length - window + 1
+
+    //PRETTY SURE THIS IS WRONG
+    var referenceArray: Array[Array[Double]] = Array.ofDim[Double](numWindows,2)
+
+    //Iterate through the list of signals, and find the mean / std dev for each window
+    for (i <- 0 until numWindows) {
+      var sum: Double = 0.0
+      //find mean
+      for (j <- i until i + window) {
+        sum += eventArray(j)
+      }
+      val mean: Double = sum / window
+      var variance: Double = 0.0
+      //find stdev
+      for (j <- i until i + window) {
+        variance += Math.pow(mean-eventArray(j),2)
+      }
+      val stddev: Double = Math.pow(variance/window,.5)
+      //populate referenceArray
+      referenceArray(i) = Array(mean, stddev)
+    }
+    referenceArray
+  }
+
+  //Helper function to create an array of t-values, so we don't need to calculate them twice
+  //Parameters: signalArray: Array of Doubles, refArray: Array of Array of doubles, window: Int
+  //Returns an array of doubles representing t values for each sliding window of window*2
+
+  def makeTArray(signalArray: Array[Double], refArray: Array[Array[Double]], window: Int): Array[Double] = {
+
+    //Number of t values
+    val numT: Int = signalArray.length - (window * 2) + 1
+    var tArray: Array[Double] = new Array[Double](numT)
+
+    for (i <- 0 until numT) {
+      val index1: Int = i           //index of first window
+      val index2: Int = i + window  //index of second window
+      //calculate the pooled standard deviation
+      val pooledSDsq: Double = ((window-1)*Math.pow(refArray(index1)(1),2) + (window-1)*Math.pow(refArray(index2)(1),2)) / ((window*2)-2)
+      val pooledSD: Double = Math.pow(pooledSDsq,.5)
+      val tValue: Double = Math.abs((refArray(index1)(0) - refArray(index2)(0)) / (pooledSD * Math.pow(2/window.toDouble,.5)))
+      println ("T Value: " + tValue)
+      tArray(i) = tValue
+    }
+    tArray
+  }
+
+  //Helper function to determine whether there is a boundary at point b
+  //Parameters will be int b as the potential boundary, the reference array refArray holding the mean/sd, tArray holding t values to
+  //minimize repetitive calculations, the window size window, threshold value threshold
+  //and peak height value ph
+  def isBoundary (b: Int, tArray: Array[Double], window: Int, threshold: Double, ph: Double ): Boolean = {
+
+    var answer: Boolean = false
+
+    //We need to convert the boundary index value into the respective index values for refArray, as well as the index value for tArray
+    val tArrayIndex: Int = b - window
+
+    //first make sure we're not looking at the first or last windows, since they can't be local peaks
+    if (!(b <= window) && !(b>=tArray.length-1)){
+      //next check to see if this is local peak
+      if ((Math.abs(tArray(tArrayIndex-1) - tArray(tArrayIndex)) >= ph) && (Math.abs(tArray(tArrayIndex+1) - tArray(tArrayIndex)) >= ph)) {
+        //finally, check if the t value is greater than the threshold
+        if (Math.abs(tArray(tArrayIndex)) >= threshold) {
+          answer = true
+        }
+      }
+    }
+    answer
+  }
+
+  // Helper to create a new list of doubles representing the signal array after segmentation using the boundaries
+  def makeEventList (signalArray: Array[Double], boundaryList: List[Int]): List[Double] = {
+
+    var tempList = boundaryList
+    var eventList: List[Double] = List()
+
+    var sum: Double = 0
+
+    //first element
+    for (i <- 0 until tempList.head) {
+      sum += signalArray(i)
+    }
+    eventList = List(sum/ tempList.head)
+
+    //middle elements
+    while (tempList.size > 1) {
+      sum = 0
+      for (i <- tempList.head until tempList(1)) {
+        sum += signalArray(i)
+      }
+      eventList = sum/(tempList(1) - tempList.head) :: eventList
+      tempList = tempList.drop(1)
+    }
+
+    //last element
+    sum = 0
+    for (i <- tempList.head until signalArray.length) {
+      sum += signalArray(i)
+    }
+    eventList = sum/(signalArray.length - tempList.head) :: eventList
+
+    eventList
+  }
+
+
   //takes a fast5 file, and converts it into an array of vectors
   def fast5ToVector (k: Int, kmerMap: Map [String,Double], filename: String, dim: Int): Array[Vector] = {
 
     //First, Z-normalize the signals
-    val eventArray: List[Double] = f5ToListZ(filename)
+    val signalList: List[Double] = f5ToListZ(filename)
+
+    //I think converting to an array makes sense, since we will be accessing it by index
+    val signalArray: Array[Double] = signalList.toArray
+
+    //Parameters
+    val window1: Int = 5
+    val window2: Int = 4
+    val threshold1: Double = 2.5
+    val threshold2: Double = 3.3
+    val peakHeight: Double = .2
+
+    //Populate reference arrays
+    val w1RefArray: Array[Array[Double]] = makeReferenceArray(signalArray,window1)
+    val w2RefArray: Array[Array[Double]] = makeReferenceArray(signalArray,window2)
+
+    //Populate t arrays
+    val w1TArray: Array[Double] = makeTArray(signalArray,w1RefArray,window1)
+    val w2TArray: Array[Double] = makeTArray(signalArray,w2RefArray,window2)
+
+    //Create a list of boundaries
+    var boundaryList: List[Int] = List()
+
+    //Populate boundaryList with boundaries
+    //We will define the boundary as the index of the first element of window2
+    for (b <- Math.min(window1,window2)  to (signalArray.length - Math.min(window1, window2))) {
+      if (isBoundary(b, w1TArray, window1, threshold1, peakHeight) || isBoundary(b, w2TArray, window2, threshold2, peakHeight)) {
+        boundaryList = b :: boundaryList
+      }
+    }
+    boundaryList = boundaryList.reverse
+
+    println ("Boundary list length: " + boundaryList.length)
+
+    val eventList: List[Double] = makeEventList(signalArray, boundaryList)
 
     //Calculate the total no of kmers and vectors
-    val kmerNo: Int = eventArray.length - k + 1
-    val vectorNo: Int = eventArray.length - k - dim + 2
+    //val kmerNo: Int = eventList.length - k + 1
+    val vectorNo: Int = eventList.length - k - dim + 2
 
     val vectorSeq: Array[Vector] = new Array[Vector](vectorNo)
 
@@ -61,11 +206,12 @@ object Fast5Process {
     for (i <- 0 until vectorNo) {
       val tempArray: Array[Double] = new Array[Double](dim)
       for (j <- 0 until dim) {
-        tempArray(j) = eventArray(i+j)
+        tempArray(j) = eventList(i+j)
       }
       vectorSeq(i) = new Vector(i, tempArray)
     }
     vectorSeq
+
   }
 
 }
--- src/test/PreProcessTest.scala
+++ src/test/PreProcessTest.scala
@@ -1,7 +1,9 @@
 package test
 
+import helper.fileToMapZ
 import org.scalatest._
 import preprocess._
+import index._
 
 class PreProcessTest extends FunSuite {
 
@@ -11,7 +13,20 @@ class PreProcessTest extends FunSuite {
 
     val testList: List[Double] = Fast5Process.f5ToListZ(filename)
     for (i <- 0 to 100) {
-      println(testList(i))
+      //println(testList(i))
     }
   }
+
+  test ("convert to vector") {
+
+    val filename: String = "data/00.txt"
+    val dim: Int = 6
+
+    val kmerModel: String = "data/6merModel.txt"
+    val kmerMap: Map[String, Double] = fileToMapZ(kmerModel)
+
+    val testArray: Array[Vector] = Fast5Process.fast5ToVector(6, kmerMap, filename, dim)
+
+  }
+
 }
